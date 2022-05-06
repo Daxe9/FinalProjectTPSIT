ce515c2e3d1b6bd26cdf04646a6bfa349118b9b4
Daxe9 <davidexie694@gmail.com>
2022-05-06 19:48:19 +0200

    added homepage decoration

--- src/components/CoinCard.vue
+++ src/components/CoinCard.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-defineProps<{
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+const props = defineProps<{
     info: {
         price_change_percentage_24h: number;
         total_volume: number;
@@ -10,30 +13,57 @@ defineProps<{
     };
     index: number;
 }>();
+
+const trend: number = props.info.price_change_percentage_24h;
+function onClick() {
+    router.push({
+        name: "CoinDetails",
+        params: {
+            coinID: 'bitcoin'
+        }
+    })
+}
 </script>
 
 <template>
     <div
         :style="{
-            borderColor: info.price_change_percentage_24h > 0 ? 'green' : 'red'
+            borderColor: trend > 0 ? 'green' : 'red'
         }"
-        class="custom-card d-flex justify-content-between w-75 mx-auto my-2">
-        <div class="d-flex justify-content-start">
-            <h1>{{ index }}. {{ info.name }}</h1>
-            <p>{{ info.price_change_percentage_24h }}%</p>
+        class="custom-card p-3 m-2"
+        @click="onClick"
+    >
+        <div class="d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+                <span class="h1 m-0">{{ index }}.</span>
+                <img class="custom-image" :src="info.image" :alt="info.name" />
+
+                <span class="h1 m-0">{{ info.name }}</span>
+            </div>
+            <div class="d-flex">
+                <span class="h3">{{ info.current_price }}$</span>
+                <span
+                    class="h6"
+                    :style="{
+                    color: trend > 0 ? 'green' : 'red'
+                }"
+                >&nbsp;{{ trend > 0 ? "+" : "-" }}{{ Math.abs(trend) }}%</span
+                >
+            </div>
         </div>
-        <img :src="info.image" :alt="info.name" />
     </div>
 </template>
 
 <style scoped>
 div img {
-    max-width: 5em;
+    margin: 0 0.4em;
+    max-width: 2.8em;
 }
 
 .custom-card {
-    padding: 1.2em;
-    border: 2px solid grey;
+    margin: 0.2em;
+    box-sizing: border-box;
+    border: 1px solid grey;
     border-radius: 2em;
 }
 </style>
