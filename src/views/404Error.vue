e8409a9d9cd9bfdb0ca74fccd95187e490f1394f
Daxe9 <davidexie694@gmail.com>
2022-05-07 16:15:54 +0200

    changed 404 error page

--- src/views/404Error.vue
+++ src/views/404Error.vue
@@ -1,31 +1,39 @@
-<script setup lang="ts">
-import sadFace from "../assets/sad-face.png";
+<script lang="ts" setup>
+import sadFace from "../assets/satoshi-nakamoto.png";
+
 defineProps<{
     coinName: string;
 }>();
 </script>
 <template>
     <div
-        title="click me mf"
-        class="d-flex flex-row justify-content-center align-items-center">
+        class="d-flex flex-row justify-content-center align-items-center"
+        title="click me mf">
         <router-link
             :to="{ name: 'Home' }"
-            class="text-decoration-none custom-link">
-            <p>Click here will bring you to the home</p>
-            <img class="custom-image" :src="sadFace" alt="a sad face" />
-            <h3>*{{ coinName || "Resource" }}* not found</h3>
+            class="text-decoration-none custom-link py-3">
+            <p>Click here, we'll bring you to the home</p>
+            <div class="d-flex flex-row-reverse justify-content-center align-items-end">
+                <img :src="sadFace" alt="a sad face" class="custom-image" />
+                <div>
+                    <p class="mb-0 custom-subtitle">Satoshi disapproves</p>
+                    <h3>*{{ coinName || "Resource" }}* not found</h3>
+                </div>
+            </div>
         </router-link>
     </div>
 </template>
 <style scoped>
 .custom-link {
-    padding: 2em;
     border: 1px solid #808080;
     border-radius: 1em;
     color: #404040;
 }
 
 .custom-image {
-    max-width: 50%;
+    max-width: 30%;
+}
+.custom-subtitle {
+    font-size: 0.85em;
 }
 </style>
